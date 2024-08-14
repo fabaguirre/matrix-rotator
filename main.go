53fa0ea975e768235ae6a40f6b4e71863000d68d
Fabs <gianfranco.aguirre.cruz@gmail.com>
2024-08-14 12:18:39 -0500

    ✨ add /status endpoint to return server status

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.SendString("Hello, World!")
+	app.Get("/status", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(&fiber.Map{
+			"success": true,
+			"message": "Server is running",
+			"status":  200,
+		})
 	})
 
 	app.Listen(":4000")
