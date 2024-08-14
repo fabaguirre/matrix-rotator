654f9f386b6dba2b302c24d9c080727b3568229d
Fabs <gianfranco.aguirre.cruz@gmail.com>
2024-08-14 18:02:13 -0500

    ✨ add QR decomposition function

--- internal/matrix/controller.go
+++ internal/matrix/controller.go
@@ -23,9 +23,18 @@ func RotateMatrixHandler(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to send data to Node.js API"})
 	}
 
+	Q, R, err := QRDecomposition(req.Matrix)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to compute QR decomposition"})
+	}
+
 	return c.Status(fiber.StatusOK).JSON(&fiber.Map{
 		"success": true,
 		"data": fiber.Map{
+			"qrDecomposition": &fiber.Map{
+				"Q": Q.RawMatrix().Data,
+				"R": R.RawMatrix().Data,
+			},
 			"rotatedMatrix": rotatedMatrix,
 			"stats":         stats,
 		},
--- internal/matrix/service.go
+++ internal/matrix/service.go
@@ -1,5 +1,9 @@
 package matrix
 
+import (
+	"gonum.org/v1/gonum/mat"
+)
+
 func RotateMatrix(matrix [][]int) [][]int {
 	rows := len(matrix)
 	cols := len(matrix[0])
@@ -16,4 +20,30 @@ func RotateMatrix(matrix [][]int) [][]int {
 	}
 
 	return result
+}
+
+func QRDecomposition(matrix [][]int) (
+	*mat.Dense, *mat.Dense, error,
+) {
+	rows := len(matrix)
+	cols := len(matrix[0])
+	data := make([]float64, rows*cols)
+
+	for i := range matrix {
+		for j := range matrix[i] {
+			data[i*cols+j] = float64(matrix[i][j])
+		}
+	}
+
+	A := mat.NewDense(rows, cols, data)
+
+	var qr mat.QR
+	qr.Factorize(A)
+
+	Q := mat.NewDense(rows, rows, nil)
+	R := mat.NewDense(rows, cols, nil)
+	qr.QTo(Q)
+	qr.RTo(R)
+
+	return Q, R, nil
 }
